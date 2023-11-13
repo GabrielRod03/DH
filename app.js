0688e7463ec045b4c7b3c317e115a8bbf541aa54
leandro22371@gmail.com <leandro22371@gmail.com>
2023-11-13 20:12:57 -0300

    config: servidor remoto

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const path = require ('path');
 const publicPath = path.resolve (__dirname, './public');
 app.use (express.static(publicPath));
 
-app.listen(3000, ()=> {
+/* app.listen(3000, ()=> {
     console.log('Servidor corriendo en el puerto 3000');
-});
+}); */
+
+let port = process.env.PORT || 3000;
+app.listen(port, () =>console.log(`servidor corriendo en el puerto ${port}`))
 
 app.get ('/', (req,res) =>{
     res.sendFile ( path.resolve(__dirname, './views/home.html'))
